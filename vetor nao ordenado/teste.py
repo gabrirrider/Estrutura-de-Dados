e2c6f4bada9bd0384436d652419e84830abe2422
gabrirrider <gamarcelino@hotmail.com>
2023-12-08 15:02:09 -0300

    git ignore

---
+++ vetor nao ordenado/teste.py
@@ -0,0 +1,11 @@
+import vetor_nao_ordenado as vno
+
+vetor = vno.VetorNaoOrdenado(5)
+vetor.imprime()
+vetor.insere(2)
+vetor.insere(2)
+vetor.insere(2)
+vetor.insere(2)
+vetor.imprime()
+vetor.insere(2)
+vetor.imprime()
